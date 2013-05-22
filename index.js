17478e23678aa079e3d970a4d9543ddc6bc84b09
zhangyunlong <zhangyunlong@baidu.com>
2013-05-22 14:33:22 +0800

    优化报错信息

--- index.js
+++ index.js
@@ -10,7 +10,13 @@ var UglifyJS = require('uglify-js');
 module.exports = function(content, file, conf){
     conf.fromString = true;
     UglifyJS.AST_Node.warn_function = function(txt){
-        fis.log.error(txt)
+        var pos = txt.indexOf('?');
+        if(pos === -1){
+            txt += ' filename: ' + file.subpath;
+        } else {
+            txt = txt.substring(0, pos) + file.subpath + txt.substring(0, pos + 1);
+        }
+        fis.log.error(txt);
     };
     return UglifyJS.minify(content, conf).code;
 };
