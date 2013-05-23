251eae685037dcaad056a5e3992a6739a64e2d41
zhangyunlong <zhangyunlong@baidu.com>
2013-05-23 22:23:10 +0800

    升级对uglify-js的依赖

--- index.js
+++ index.js
@@ -9,14 +9,5 @@ var UglifyJS = require('uglify-js');
 
 module.exports = function(content, file, conf){
     conf.fromString = true;
-    UglifyJS.AST_Node.warn_function = function(txt){
-        var pos = txt.indexOf('?');
-        if(pos === -1){
-            txt += ' filename: ' + file.realpath;
-        } else {
-            txt = txt.substring(0, pos) + file.realpath + txt.substring(pos + 1);
-        }
-        fis.log.error(txt);
-    };
     return UglifyJS.minify(content, conf).code;
 };
