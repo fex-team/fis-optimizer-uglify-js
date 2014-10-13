273cde602e9d03b903e292408d4b666c05f26baf
liaoxuezhi <liaoxuezhi@baidu.com>
2014-10-13 10:57:51 +0800

    add a switcher for sourcemap

--- index.js
+++ index.js
@@ -8,38 +8,42 @@
 var UglifyJS = require('uglify-js');
 
 module.exports = function(content, file, conf){
-
-    var mapping = fis.file.wrap(file.dirname + '/' + file.filename + '.map');
-
     conf.fromString = true;
-    conf.outSourceMap = file.filename + '.org' + file.rExt;
+
+    if (conf.sourceMap) {
+        var mapping = fis.file.wrap(file.dirname + '/' + file.filename + '.map');
+        conf.outSourceMap = file.filename + '.org' + file.rExt;
+    }
 
     var ret = UglifyJS.minify(content, conf);
 
-    mapping.useDomain = true;
-    mapping.useHash = true;
+    if (conf.sourceMap) {
+        mapping.useDomain = true;
+        mapping.useHash = true;
 
-    var mapData = JSON.parse(ret.map);
+        var mapData = JSON.parse(ret.map);
 
-    mapData.sources = [mapData.file];
-    mapData.sourcesContent = [content];
+        mapData.sources = [mapData.file];
+        mapData.sourcesContent = [content];
 
-    var newData = {
-        version: mapData.version,
-        file: mapData.file,
-        sources: mapData.sources,
-        sourcesContent: mapData.sourcesContent,
-        names: mapData.names,
-        mappings: mapData.mappings
-    };
+        var newData = {
+            version: mapData.version,
+            file: mapData.file,
+            sources: mapData.sources,
+            sourcesContent: mapData.sourcesContent,
+            names: mapData.names,
+            mappings: mapData.mappings
+        };
 
-    mapping.setContent(JSON.stringify(newData));
+        mapping.setContent(JSON.stringify(newData));
 
-    file.extras = file.extras || {};
-    file.extras.derived = file.extras.derived || [];
-    file.extras.derived.push(mapping);
+        file.extras = file.extras || {};
+        file.extras.derived = file.extras.derived || [];
+        file.extras.derived.push(mapping);
 
-    ret.code += '\n//# sourceMappingURL={{url:'+mapping.subpath + '}}\n';
+        ret.code += '\n//# sourceMappingURL={{url:'+mapping.subpath + '}}\n';
+    }
+
 
     return ret.code;
 };
