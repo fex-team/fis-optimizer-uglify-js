b6210f4a64af9197757cc3b10c53557fc5d3786e
2betop <liaoxuezhi@icloud.com>
2015-06-14 11:32:19 +0800

    压缩失败不应该报错，而是 warn

--- index.js
+++ index.js
@@ -7,10 +7,130 @@
 
 var UglifyJS = require('uglify-js');
 
+function uglify(content, file, conf) {
+  conf.fromString = true;
+
+  if (conf.sourceMap) {
+      var mapping = fis.file.wrap(file.dirname + '/' + file.filename + '.map');
+      conf.outSourceMap = file.filename + '.org' + file.rExt;
+  }
+
+  var ret = UglifyJS.minify(content, conf);
+
+  if (conf.sourceMap) {
+      mapping.useDomain = true;
+      mapping.useHash = true;
+
+      var mapData = JSON.parse(ret.map);
+
+      mapData.sources = [mapData.file];
+      mapData.sourcesContent = [content];
+
+      var newData = {
+          version: mapData.version,
+          file: mapData.file,
+          sources: mapData.sources,
+          sourcesContent: mapData.sourcesContent,
+          names: mapData.names,
+          mappings: mapData.mappings
+      };
+
+      mapping.setContent(JSON.stringify(newData));
+
+      file.extras = file.extras || {};
+      file.extras.derived = file.extras.derived || [];
+      file.extras.derived.push(mapping);
+
+      ret.code += '\n//# sourceMappingURL=' + mapping.getUrl(fis.compile.settings.hash, fis.compile.settings.domain); + '\n';
+  }
+
+  return ret.code;
+}
+
+module.exports = function(content, file, conf){
+
+  try {
+    content = uglify(content, file, conf);
+  } catch (e) {
+    fis.log.warn('Got Error %s while uglify %s', e.message, file.subpath);
+    fis.log.debug(e.stack);
+  }
+
+  return content;
+};
+/*
+ * fis
+ * http://fis.baidu.com/
+ */
+
+'use strict';
+
+var UglifyJS = require('uglify-js');
+
 module.exports = function(content, file, conf){
     conf.fromString = true;
     if (conf.sourceMap) {
-        conf.outSourceMap = file.filename + '.org' + file.rExt;
+        conf.outSourceMap =/*
+ * fis
+ * http://fis.baidu.com/
+ */
+
+'use strict';
+
+var UglifyJS = require('uglify-js');
+
+function uglify(content, file, conf) {
+  conf.fromString = true;
+
+  if (conf.sourceMap) {
+      var mapping = fis.file.wrap(file.dirname + '/' + file.filename + '.map');
+      conf.outSourceMap = file.filename + '.org' + file.rExt;
+  }
+
+  var ret = UglifyJS.minify(content, conf);
+
+  if (conf.sourceMap) {
+      mapping.useDomain = true;
+      mapping.useHash = true;
+
+      var mapData = JSON.parse(ret.map);
+
+      mapData.sources = [mapData.file];
+      mapData.sourcesContent = [content];
+
+      var newData = {
+          version: mapData.version,
+          file: mapData.file,
+          sources: mapData.sources,
+          sourcesContent: mapData.sourcesContent,
+          names: mapData.names,
+          mappings: mapData.mappings
+      };
+
+      mapping.setContent(JSON.stringify(newData));
+
+      file.extras = file.extras || {};
+      file.extras.derived = file.extras.derived || [];
+      file.extras.derived.push(mapping);
+
+      ret.code += '\n//# sourceMappingURL=' + mapping.getUrl(fis.compile.settings.hash, fis.compile.settings.domain); + '\n';
+  }
+
+  return ret.code;
+}
+
+module.exports = function(content, file, conf){
+
+  try {
+    content = uglify(content, file, conf);
+  } catch (e) {
+    fis.log.warn('Got Error %s while uglify %s', e.message, file.subpath);
+    fis.log.debug(e.stack);
+  }
+
+  return content;
+};
+ file.filename + '.org' + file.rExt;
     }
 
     var ret = UglifyJS.minify(content, conf);
